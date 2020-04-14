ebdeba0da4a37db29a42d2a22f50b702387bb442
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-14 17:58:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_index_page(index_url) # Responsible for scraping the index page that lists all of the students
     students = []
     html = open(index_url)
     index = Nokogiri::HTML(html)
