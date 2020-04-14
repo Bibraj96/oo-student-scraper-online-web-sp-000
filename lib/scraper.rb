e7b263bc3d17904bdce659fd458455782f3886d1
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-14 17:36:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,9 @@ class Scraper
   def self.scrape_index_page(index_url) #Responsible for scraping the index page that lists all of the students
     students = []
     html = open(index_url)
+    index.css("div.student-card").each do |student|
+      student_details = {}
+      
   end
 
   def self.scrape_profile_page(profile_url) #Responsible for scraping an individual student's profile page to get further information
