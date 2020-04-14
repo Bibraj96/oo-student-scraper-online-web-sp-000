42df54dc21d8a1c3398c781e2217b61700301010
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-14 16:49:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,12 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) #Responsible for scraping the index page that lists all of the students
+
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  def self.scrape_profile_page(profile_url) #Responsible for scraping an individual student's profile page to get further information
+
   end
 
 end
-
