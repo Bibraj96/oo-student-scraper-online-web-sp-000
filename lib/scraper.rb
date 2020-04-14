1dbe0db3273dbd0d565dc1c0a3d1f919ecffcffa
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-14 17:45:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,6 +12,7 @@ class Scraper
       student_details[:location] = student.css("p.student-location").text
       profile_path = student.css("a").attribute("href").value
       student_details[:profile_url] = './fixtures/student-site/' + profile_path
+      students << student_details
   end
 
   def self.scrape_profile_page(profile_url) #Responsible for scraping an individual student's profile page to get further information
